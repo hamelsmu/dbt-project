b493654ad126b9b4e498f746b87a402851328c08
Hamel Husain <hamel.husain@gmail.com>
2023-07-17 16:14:21 +0000

    modularity

---
+++ models/stg_customers.sql
@@ -0,0 +1,13 @@
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from dbt-tutorial.jaffle_shop.customers
+
+)
+
+
+select * from customers
