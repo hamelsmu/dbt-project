5f8532292d0d5db9cad01ffd70a0ca809224e768
Hamel Husain <hamel.husain@gmail.com>
2023-07-17 17:55:25 +0000

    configure sources

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,9 +5,8 @@ with customers as (
         first_name,
         last_name
 
-    from dbt-tutorial.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers')}}
 
 )
-
 
 select * from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -6,7 +6,7 @@ with orders as (
         order_date,
         status
 
-    from dbt-tutorial.jaffle_shop.orders
+    from {{ source('jaffle_shop', 'orders') }}
 
 )
 
